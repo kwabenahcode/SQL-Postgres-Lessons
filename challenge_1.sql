1c1cd09af3ef261c2441b27a42022a67b31dae94
kwabenahcode <mrofori507@gmail.com>
2026-01-27 02:14:14 +0000

    first commit

---
+++ challenge_1.sql
@@ -0,0 +1,22 @@
+SELECT COUNT(*) FROM payment
+WHERE amount > 5;
+
+SELECT COUNT(*) FROM actor
+WHERE first_name LIKE 'P%';
+
+SELECT COUNT(DISTINCT(district)) FROM address;
+
+SELECT DISTINCT district 
+FROM address;
+
+SELECT COUNT(*) FROM film
+WHERE rating = 'R' AND replacement_cost BETWEEN 5 AND 15;
+
+SELECT COUNT(*) FROM film
+WHERE title ILIKE '%truman%';
+
+
+
+
+
+
